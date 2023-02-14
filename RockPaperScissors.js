447fde16fb1ff6b0a432709d05253626b534befc
laczarade <billjavandel@gmail.com>
2023-02-13 22:19:00 -0800

    Add game() method

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -26,7 +26,36 @@ function playRound(playerSelection, computerSelection) {
 		return `It's a tie! Both players have chosen ${playerSelection}`;
 	}
 }
-
+/*
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+*/
+
+function game() {
+	let playerScore = 0;
+	let computerScore = 0;
+	for (let i = 0; i < 5; i++) {
+		let playerSelection = prompt("Make your move: ");	
+		let round = playRound(playerSelection, getComputerChoice());
+		switch(round.substring(0, 8)) {
+			case "You win":
+				playerScore++;
+				break;
+			case "You los":
+				computerScore++;
+				break;
+			default:
+				break;
+		}
+		console.log(round);
+	}
+	console.log(`Player: ${playerScore} \nComputer: ${computerScore}`)
+	if (playerScore > computerScore) {
+		console.log("You win!");
+	} else if (playerScore < computerScore) {
+		console.log("Computer wins!");
+	} else {
+		console.log("It's a tie!");
+	}
+}
