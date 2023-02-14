5abfe65e17761a3cec760166b24747a758debea4
laczarade <billjavandel@gmail.com>
2023-02-13 22:02:02 -0800

    Add playRound method

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,3 +1,4 @@
+"use strict"
 function getComputerChoice() {
 	switch (Math.floor(Math.random() * 3)) {
 		case 0:
@@ -12,4 +13,16 @@ function getComputerChoice() {
 	}
 }
 
+const victories = [["Paper", "Rock"], ["Scissors", "Paper"], ["Rock", "Scissors"]];
 
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase(); //I hope this works
+	console.log(playerSelection);
+	if (victories.includes([playerSelection, computerSelection])) {
+		return `You win! ${playerSelection} beats ${computerSelection}`;	
+	} else if (victories.includes([computerSelection, playerSelection])) {
+		return `You lose! ${computerSelection} beats ${playerSelection}`;
+	} else {
+		return `It's a tie! Both players have chosen ${playerSelection}`;
+	}
+}
